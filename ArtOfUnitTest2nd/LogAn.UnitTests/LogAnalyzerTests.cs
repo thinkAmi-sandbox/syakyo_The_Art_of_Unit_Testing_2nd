cdbc883296527d8f5545fb5d1ee1b1ebd48486ec
thinkAmi <dev.thinkami+git@gmail.com>
2015-05-04 07:04:50 +0900

    2.5 Refactoring to parameterized tests

--- ArtOfUnitTest2nd/LogAn.UnitTests/LogAnalyzerTests.cs
+++ ArtOfUnitTest2nd/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -10,6 +10,7 @@ namespace LogAn.UnitTests
 {
     public class LogAnalyzerTests
     {
+        // 引数なしの時のテスト
         [Fact]
         public void IsValidFileName_BadExtension_ReturnsFalse()
         {
@@ -20,7 +21,9 @@ namespace LogAn.UnitTests
             Assert.False(result);
         }
 
-        [Fact]
+        // `IsValidLogFileName_ValidExtensions_ReturnsTrue`メソッドがあるので、
+        // このメソッドはスキップする
+        [Fact(Skip = "Use `IsValidLogFileName_ValidExtensions_ReturnsTrue` method")]
         public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
         {
             var analyzer = new LogAnalyzer();
@@ -29,7 +32,7 @@ namespace LogAn.UnitTests
             Assert.True(result);
         }
 
-        [Fact]
+        [Fact(Skip = "Use `IsValidLogFileName_ValidExtensions_ReturnsTrue` method")]
         public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
         {
             var analyzer = new LogAnalyzer();
@@ -37,5 +40,56 @@ namespace LogAn.UnitTests
 
             Assert.True(result);
         }
+
+        // データを与えたい時、FactのかわりにTheoryを使う
+        [Theory]
+        [InlineData("filewithgoodextension.SLF")]
+        [InlineData("filewithgoodextension.slf")]
+        public void IsValidLogFileName_ValidExtensions_ReturnsTrue(string file)
+        {
+            var analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.True(result);
+        }
+
+        // xUnit.net v2から、PropertyDataがMemberDataへと変更
+        // 静的プロパティのほか、静的メソッドや静的メンバ変数も利用可能
+        [Theory]
+        [MemberData("StaticPropertyTestData")]
+        [MemberData("StaticMethodTestData")]
+        public void IsValidLogFileName_ValidExtensions_ChecksThem(string file, bool expected)
+        {
+            var analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.Equal(expected, result);
+        }
+
+        // 静的プロパティ版
+        public static IEnumerable<object> StaticPropertyTestData
+        {
+            get
+            {
+                return new[] {
+                    new object[] { "filewithgoodextension_property.SLF", true },
+                    new object[] { "filewithgoodextension_property.slf", true },
+                    new object[] { "filewithgoodextension_property.foo", false },
+                };
+            }
+        }
+
+        // 静的メンバ版
+        public static IEnumerable<object> StaticMethodTestData()
+        {
+            return new[] {
+                    new object[] { "filewithgoodextension_method.SLF", true },
+                    new object[] { "filewithgoodextension_method.slf", true },
+                    new object[] { "filewithgoodextension_method.foo", false },
+                };
+        }
+
     }
 }
